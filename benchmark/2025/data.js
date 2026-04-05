193475e5c2a862b9e1de0fbd11abd78397b695f4
github-action-benchmark <github@users.noreply.github.com>
2026-04-05 05:56:02 +0000

    add Tabularray Benchmark in TeX Live 2025 (customSmallerIsBetter) benchmark result for c6e57339096bd6b77800240e7dfe2a7aa35fe6c3

--- benchmark/2025/data.js
+++ benchmark/2025/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775338526613,
+  "lastUpdate": 1775368562313,
   "repoUrl": "https://github.com/TeXackers/tabularray",
   "entries": {
     "Tabularray Benchmark in TeX Live 2025": [
@@ -42,6 +42,48 @@ window.BENCHMARK_DATA = {
             "value": 1.0269112589298,
             "unit": "ratio",
             "extra": "current time : previous time = 1.6242218017578 : 1.581657409668"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "73948299+plante3@users.noreply.github.com",
+            "name": "plante",
+            "username": "plante3"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c6e57339096bd6b77800240e7dfe2a7aa35fe6c3",
+          "message": "Merge pull request #640 from muzimuzhi/chore/gh-pages-links\n\nchore: update GitHub Pages links",
+          "timestamp": "2026-04-05T13:54:51+08:00",
+          "tree_id": "0cb390fda2f453707d85f60aeb52ef16075cb321",
+          "url": "https://github.com/TeXackers/tabularray/commit/c6e57339096bd6b77800240e7dfe2a7aa35fe6c3"
+        },
+        "date": 1775368561615,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "compile tabularray with pdflatex",
+            "value": 0.99281479155507,
+            "unit": "ratio",
+            "extra": "current time : previous time = 1.0251998901367 : 1.0326194763184"
+          },
+          {
+            "name": "compile tabularray with xelatex",
+            "value": 1.0006500282249,
+            "unit": "ratio",
+            "extra": "current time : previous time = 1.1157417297363 : 1.1150169372559"
+          },
+          {
+            "name": "compile tabularray with lualatex",
+            "value": 0.98665784050693,
+            "unit": "ratio",
+            "extra": "current time : previous time = 1.6090888977051 : 1.6308479309082"
           }
         ]
       }
